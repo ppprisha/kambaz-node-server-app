85b7cf4a839ced3a003014a0329f4e858c3be325
ppprisha <prisha.sujin@gmail.com>
2025-08-17 18:55:22 -0400

    fix

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import session from 'express-session';
+import session from "express-session";
 import "dotenv/config";
 
-import { Hello } from './Hello.js';
-import Lab5 from "./Lab5/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
-import CourseRoutes from './Kambaz/Courses/routes.js';
-import AssignmentRoutes from './Kambaz/Assignment/routes.js';
-import EnrollmentsRoutes from './Kambaz/Enrollments/routes.js';
+import CourseRoutes from "./Kambaz/Courses/routes.js";
+import AssignmentRoutes from "./Kambaz/Assignment/routes.js";
+import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: process.env.NETLIFY_URL.replace(/\/$/, ""), credentials: true,
-    })
+  cors({
+    origin: process.env.NETLIFY_URL.replace(/\/$/, ""), // frontend URL
+    credentials: true, 
+  })
 );
 
 app.use(express.json());
@@ -27,13 +26,9 @@ const sessionOptions = {
   cookie: {
     sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
     secure: process.env.NODE_ENV === "development" ? false : true,
-    domain: process.env.NODE_ENV === "development" ? undefined : process.env.NODE_SERVER_DOMAIN,
   },
 };
-
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-}
+if (process.env.NODE_ENV !== "development") sessionOptions.proxy = true;
 
 app.use(session(sessionOptions));
 
@@ -41,10 +36,6 @@ UserRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
 EnrollmentsRoutes(app);
-Lab5(app);
-Hello(app);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
