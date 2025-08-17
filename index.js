9571d20fb8015fab156e0950504f2f25b7964fa2
ppprisha <prisha.sujin@gmail.com>
2025-08-17 18:22:22 -0400

    fix

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL,
+    origin: process.env.NETLIFY_URL, 
+    credentials: true,                
   })
 );
 
@@ -25,15 +25,16 @@ const sessionOptions = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  cookie: {
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  },
 };
+
 if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
-  };
+  sessionOptions.proxy = true; 
 }
+
 app.use(session(sessionOptions));
 
 UserRoutes(app);
